a56c05d55262c414f5951368133dc074aac45ed7
Roberta <robertaokada144@gmail.com>
2023-11-29 16:11:41 -0300

    first

---
+++ src/todo/tasks.service.ts
@@ -0,0 +1,48 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { TaskModel } from './task.model';
+
+@Injectable()
+export class TasksService {
+  _tasks: TaskModel[] = [
+    {id: 1, description: "Feed the cat"},
+    {id: 2, description: "Wash the dishes"},
+    {id: 3, description: 'Take Helena to robotic class'}
+  ]
+
+  findAllTasks(): TaskModel[]{
+    return this._tasks;
+  }
+
+  findTaskById(id: number): TaskModel{
+    return this._tasks.find(t => t.id == id);
+  }
+
+  createTask(t: TaskModel): TaskModel{
+    let taskExists = this._tasks.filter(task => task.id == t.id);
+    if(taskExists.length == 0){
+      this._tasks.push(t);
+      return t;
+    }
+    //throw new Error("Already exist a task with this id");
+    
+  }
+
+  updateTask(id: number, t: TaskModel): TaskModel{
+    let taskExists = this._tasks.filter(task => task.id == id);
+    if(taskExists.length == 0) {
+      throw new NotFoundException(`Task with id ${id} not found`);
+    }
+    taskExists[0].description = t.description;
+    taskExists[0].is_complete = t.is_complete;
+    return taskExists[0];
+  }
+
+  deleteTask(id:number): TaskModel{
+    let taskIndex = this._tasks.findIndex(task => task.id === id);
+    if(taskIndex = -1) {
+      throw new NotFoundException(`Task with id ${id} not found`);
+    }
+    const deletedTask = this._tasks.splice(taskIndex, 1)[0];
+    return deletedTask;
+  }
+}
---
+++ src/todo/tasks/tasks.controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put } from '@nestjs/common';
+import { TasksService } from '../tasks.service';
+import { TaskModel } from '../task.model';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private readonly taskService: TasksService) {}
+    
+  @Get()
+  getTasks(){
+    return this.taskService.findAllTasks();
+  }
+
+  @Get(':taskId')
+  getTaskById(@Param('taskId') id: number) {
+    let task = this.taskService.findTaskById(id);
+    if(!task){
+      throw new NotFoundException(`Task with id ${id} not found`)
+    }
+    return task;
+  }
+
+  @Post()
+  createTask(@Body() t: TaskModel){
+    this.taskService.createTask(t);
+    return t;
+  }
+
+  @Put(':taskId')
+  updateTask(@Param('taskId') id: number, @Body() t: TaskModel){
+    this.taskService.updateTask(id, t);
+  }
+
+  @Delete(':taskId')
+  deleteTask(@Param('taskId') id: number){
+    this.taskService.deleteTask(id);
+  }
+}
